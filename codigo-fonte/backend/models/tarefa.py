1c1e1cf622a1bda4dfd7aca6da92f79098e4b3f3
Rafael Luis Toledo Orsolini <101680431+rafaelorsolini@users.noreply.github.com>
2025-11-17 22:31:34 -0300

    Update tarefa.py

--- codigo-fonte/backend/models/tarefa.py
+++ codigo-fonte/backend/models/tarefa.py
@@ -1,1 +1,16 @@
 
+from flask_sqlalchemy import SQLAlchemy
+from app import db # Importamos o 'db' que foi inicializado no app.py
+
+# Modelo da Tabela Tarefa (baseado no seu script SQL)
+class Tarefa(db.Model):
+    __tablename__ = 'Tarefas' # Nome exato da tabela no MySQL
+    
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    titulo = db.Column(db.String(150), nullable=False)
+    descricao = db.Column(db.Text, nullable=True)
+    status = db.Column(db.Enum('pendente', 'concluida'), default='pendente')
+    # Não precisamos da data_criacao ou usuario_id agora para simplificar
+    
+    def __repr__(self):
+        return f'<Tarefa {self.titulo}>'
