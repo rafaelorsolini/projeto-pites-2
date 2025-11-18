a62299f27f31ec0751d4f6f2447cfb93e0c311b7
Rafael Luis Toledo Orsolini <101680431+rafaelorsolini@users.noreply.github.com>
2025-11-17 22:36:18 -0300

    Update usuario.py

--- codigo-fonte/backend/models/usuario.py
+++ codigo-fonte/backend/models/usuario.py
@@ -1,1 +1,16 @@
 
+from app import db # Importamos o 'db' que foi inicializado no app.py
+
+class Usuario(db.Model):
+    __tablename__ = 'Usuarios' # Nome exato da tabela no MySQL
+    
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    nome = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    senha = db.Column(db.String(255), nullable=False) # No app real, isso seria hash
+    
+    # Define o relacionamento: Um Usuário tem muitas Tarefas
+    tarefas = db.relationship('Tarefa', backref='usuario', lazy=True)
+
+    def __repr__(self):
+        return f'<Usuario {self.nome}>'
